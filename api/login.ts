ce39ce70c5e5455f34fd6d8d2670f9eaeb114d0c
b161009 <dokimsa011984@gmail.com>
2026-04-24 23:49:55 +0700

    fix: remove socket + vercel api

--- api/login.ts
+++ api/login.ts
@@ -1,5 +1,32 @@
-import type { VercelRequest, VercelResponse } from "@vercel/node"
+export default function handler(req: any, res: any) {
+  if (req.method !== "POST") {
+    return res.status(405).end()
+  }
 
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  res.status(200).json({ message: "API is working" })
+  const { email, password } = req.body
+
+  // test tạm
+  if (email === "admin@gmail.com" && password === "123456") {
+    return res.status(200).json({
+      user: {
+        id: 1,
+        email,
+        role: "admin"
+      }
+    })
+  }
+
+  if (email === "user@gmail.com" && password === "123456") {
+    return res.status(200).json({
+      user: {
+        id: 2,
+        email,
+        role: "user"
+      }
+    })
+  }
+
+  return res.status(401).json({
+    error: "Sai tài khoản hoặc mật khẩu"
+  })
 }
